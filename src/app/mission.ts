4c51d4e8b29695f94a54cbd802b5279412216be8
qlows <79415956+qlows@users.noreply.github.com>
2023-04-03 11:40:17 -0400

    Fixed an issue where images were not being displayed in the missionlist

--- src/app/mission.ts
+++ src/app/mission.ts
@@ -3,5 +3,9 @@ export interface Mission {
     mission_name: string;
     launch_year: string;
     details: string;
-    mission_patch_small: string;
-}
+    links: {
+      mission_patch: string;
+      mission_patch_small: string;
+    };
+  }
+  
